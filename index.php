f1f233b6b9796f77da8872ad1765f801d9280f6d
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-05-07 00:47:50 +0800

    update navbar

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php require_once 'header.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php require_once 'head.php'; ?>
+</head>
+<body>
+<?php require_once 'nav.php'; ?>
+
+<?php require_once 'footer.php'; ?>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="contained-fluid">
+		<div class="navbar-header">
+			<!-- Data target should be accorded with the id of collapse div part -->
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#my-navbar-collapse">
+	            <span class="sr-only">Go to...</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	        </button>
+
+			<a href="#" class="navbar-brand">CS1101S</a>
+		</div>
+
+		<!-- Compatibility for mobile browsers, will collapse on smaller screens -->
+		<div class="collapse navbar-collapse" id="my-navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="https://www.ivle.nus.edu.sg/">IVLE</a></li>
+				<li><a href="https://sourceacademy.comp.nus.edu.sg/">Source Academy</a></li>
+				<li><a href="https://source-ide.surge.sh/">Source IDE</a></li>
+				<li><a href="https://comp.nus.edu.sg/~e0134079/">Yunpeng's Website</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<!-- Because the navigation bar is fixed at the top, we need 3 new lines to avoid being blocked by it. -->
+<br><br><br>
