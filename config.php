ae2eaf4bd7ae97d01374b3ed89a678c92af730cd
Yunpeng Niu <Beckid@users.noreply.github.com>
2017-05-27 02:01:03 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DB_TYPE', 'mysql', true);
 define('DB_HOST', 'localhost', true);
 define('DB_NAME', 'cs1101s', true);
 define('DB_UNAME', 'cs1101s', true);
-define('DB_PWORD', '86395676', true);
+define('DB_PWORD', '123456', true);
 
 // Gets the DSN to help create connection to the database.
 $dsn = DB_TYPE . ":host=" . DB_HOST . ";db_name=" . DB_NAME;
