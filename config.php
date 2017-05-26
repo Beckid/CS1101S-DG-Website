4f082a54b36ac22f5ab05f927b5365ec91d659a2
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-05-27 02:47:29 +0800

    update the configuration file for database connection.

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
 define('DB_TYPE', 'mysql', true);
 define('DB_HOST', 'localhost', true);
 define('DB_NAME', 'cs1101s', true);
+define('DB_PREFIX', 'cs1101s', true);
 define('DB_UNAME', 'cs1101s', true);
 define('DB_PWORD', '123456', true);
 
