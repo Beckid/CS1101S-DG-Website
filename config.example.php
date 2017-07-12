4dbaa43621827343aa5290c95a53c697ccab068e
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-07-12 23:12:56 +0800

    Add comments

--- config.example.php
+++ config.example.php
@@ -1,3 +1,5 @@
+<!-- Notice: Change the file name to config.php -->
+
 <?php
 // Settings that is required for connecting to the database server.
 define('DB_TYPE', 'mysql', true);
@@ -5,7 +7,7 @@ define('DB_HOST', 'localhost', true);
 define('DB_NAME', 'cs1101s', true);
 define('DB_PREFIX', 'cs1101s', true);
 define('DB_UNAME', 'root', true);
-define('DB_PWORD', '86395676', true);
+define('DB_PWORD', '123456', true);
 
 // Gets the DSN to help create connection to the database.
 $dsn = DB_TYPE . ":host=" . DB_HOST . ";db_name=" . DB_NAME;
