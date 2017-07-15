413d72fc9f0bfd73be4aba4eaabb783f6401d3d3
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-07-15 15:00:13 +0800

    Fix stype for large screen (laptop)

--- login.php
+++ login.php
@@ -32,7 +32,7 @@ if (logged_in()) {
 <h1>User Login</h1>
 
 <div class="container">
-	<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4 col-lg-4 col-lg-offset-4">
+	<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4 col-lg-6 col-lg-offset-3">
 		<form role="form" method="post" action="login.php">
 			<div id="error_message" class="">
 				<?php if ($login_fail) {
