558764da3d9f8a9b072b087eff2bac9a3f3d8c09
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-07-31 11:11:47 +0800

    automatically re-direct to HTTPS protocol

--- head.php
+++ head.php
@@ -32,3 +32,13 @@
 
 	<!-- Scripts for Google Analytics -->
 	<!-- Put the script provided by Google here. -->
+
+	<!-- Re-direct to HTTPS protocol automatically, for a secured better web. -->
+	<?php
+	if (!isset($_SERVER['HTTPS'])) {
+	    $redirect_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+	    header('HTTP/1.1 301 Moved Permanently');
+	    header("Location: $redirect_url");
+	    exit();
+	}
+	?>
