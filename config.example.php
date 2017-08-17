9e7454128cae6ed5abdbb25d5e48451192cf04c3
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-08-17 09:52:50 +0800

    Refactor the config example file

--- config.example.php
+++ config.example.php
@@ -1,8 +1,10 @@
 <!-- Notice: Change the file name to config.php -->
 
 <?php
-// Settings that are required for connecting to the database server.
+// If you are using Microsoft SQL Server, change DB_TYPE into 'sqlsrv'.
 define('DB_TYPE', 'mysql', true);
+
+// Settings that are required for connecting to the database server.
 define('DB_HOST', 'localhost', true);
 define('DB_NAME', 'cs1101s', true);
 define('DB_PREFIX', 'cs1101s', true);
@@ -10,10 +12,12 @@ define('DB_UNAME', 'root', true);
 define('DB_PWORD', '123456', true);
 
 // Gets the DSN to help create connection to the database.
-$dsn = DB_TYPE . ":host=" . DB_HOST . ";db_name=" . DB_NAME;
-// If you are using Microsoft SQL Server, use the one below instead and 
-// change DB_TYPE into 'sqlsrv'.
-// $dsn = DB_TYPE . ":server=" . DB_HOST . ";database=" . DB_NAME;
+// Notice that the DSN for Microsoft SQL Server is slightly different.
+if (DB_TYPE != 'sqlsrv') {
+	$dsn = DB_TYPE . ":server=" . DB_HOST . ";database=" . DB_NAME;
+} else {
+	$dsn = DB_TYPE . ":host=" . DB_HOST . ";db_name=" . DB_NAME;
+}
 
 define('DSN', $dsn, true);
 
