9d5ff14df9b05a45c59b8ba9597702415ee96618
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-08-18 19:32:05 +0800

    Add phpinfo page

---
+++ info.php
@@ -0,0 +1,19 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php require_once 'head.php'; ?>
+</head>
+<body>
+<?php
+require_once 'useful.php';
+
+// Only admin user can use this tool.
+if (!logged_in() || $_SESSION['usertype'] != "admin") {
+	header("location: login.php");
+} else {
+	echo phpinfo();
+}
+?>
+</body>
+</html>
