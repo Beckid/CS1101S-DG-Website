a0055e41f6eea43f8264cb20249ca90631e2438a
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-08-19 14:08:13 +0800

    Fix year in footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer style="text-align: center;">
 	<p>
-		Copyright &copy; 2017 <strong><a href="#">CS1101S DG Website 2017</a></strong>. 
+		Copyright &copy; 2017 - 2018 <strong><a href="#">CS1101S Studio Website</a></strong>. 
 		Created by <a href="https://yunpengn.github.io/">Niu Yunpeng</a>. 
 		All Rights Reserved. 
 	</p>
