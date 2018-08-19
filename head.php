01f1320792a5889caa3c8e843d1cda219b360da4
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-08-19 12:34:20 +0800

    Include GA script

--- head.php
+++ head.php
@@ -31,7 +31,15 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 	<!-- Scripts for Google Analytics -->
-	<!-- Put the script provided by Google here. -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-92228484-3', 'auto');
+		ga('send', 'pageview');
+	</script>
 
 	<!-- Re-direct to HTTPS protocol automatically, for a secured better web. -->
 	<?php
